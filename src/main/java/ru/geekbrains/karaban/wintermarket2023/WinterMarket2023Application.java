53bd6829eb9ba2a1cde44c40d718b624cb999b1a
Karaban Gleb <gleb1524@yandex.ru>
2023-01-09 15:44:59 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/karaban/wintermarket2023/WinterMarket2023Application.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.karaban.wintermarket2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinterMarket2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinterMarket2023Application.class, args);
+	}
+
+}
